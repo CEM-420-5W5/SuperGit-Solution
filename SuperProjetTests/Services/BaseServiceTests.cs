c8261beb41757d3e34a2395a96acca34dcf09817
Mathieu Briau <mathieu.briau@gmail.com>
2024-04-07 21:22:15 -0400

    Ajout d'un test (qui ne passe pas)

---
+++ SuperProjetTests/Services/BaseServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperProjet.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperProjet.Services.Tests
+{
+    [TestClass()]
+    public class BaseServiceTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
